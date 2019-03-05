38447a4048a6f507111855c43efba652ec22cee0
RockyJames <jaume@campeny.net>
2019-03-05 11:36:11 +0100

    Initial commit

---
+++ src/main/java/org/salle/GithubManager.java
@@ -0,0 +1,44 @@
+package org.salle;
+
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+
+
+public class GithubManager {
+
+
+    public static void main(String[] args) {
+
+        final String BASE_URL = "https://api.github.com/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GithubService githubService = retrofit.create(GithubService.class);
+
+        Call<List<Repo>> call = githubService.listRepos("RockyJames");
+
+        try {
+            Response<List<Repo>> response = call.execute();
+
+            if (response.isSuccessful()) {
+                System.out.println(response.body());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+
---
+++ src/main/java/org/salle/GithubService.java
@@ -0,0 +1,13 @@
+package org.salle;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
+public interface GithubService {
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);
+
+}
